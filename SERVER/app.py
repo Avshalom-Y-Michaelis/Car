3f3d754c04103c7164a2847a909769ba99a53168
Avhsalom Michaelis <avhsalom.y.m@gmail.com>
2024-08-21 13:51:17 +0300

    add base web sockets comnunication

--- SERVER/app.py
+++ SERVER/app.py
@@ -1,12 +1,26 @@
 from flask import Flask
-from servises.ObdServisesUtils import getObdSectionData
+# from flask_socketio import SocketIO, emit
+from flask_cors import CORS
+import socketio
+
+# from servises.ObdServisesUtils import getObdSectionData
 
 app = Flask(__name__)
+# app.config['SECRET_KEY'] = 'secret!'
+CORS(app,resources={r"/*":{"origins":"http://localhost:3000"}})
+# socketio = SocketIO(app,cors_allowed_origins="http://localhost:3000")
+sio = socketio.Server(cors_allowed_origins="http://localhost:3000")
+app.wsgi_app = socketio.WSGIApp(sio, app.wsgi_app)
+app.run()
+# @app.route("/data/engion")
+@sio.event
+def connect(sid, environ, auth):
+    print('connect ', sid)
+    sio.emit('my event', {'data': 'foobar'})
 
-@app.route("/data/engion")
-def getObdSectionData():
-    return getObdSectionData("engion")
 
-@app.route("/")
+    # return getObdSectionData("engion")
+
+@app.route("/a")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "hellow world"
