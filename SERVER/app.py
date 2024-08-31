2aa6600f18a27902067d6a800d9a05c8f31080d8
Avhsalom Michaelis <avhsalom.y.m@gmail.com>
2024-08-31 21:12:18 +0300

    fix git trubels

--- SERVER/app.py
+++ SERVER/app.py
@@ -10,25 +10,6 @@ app.wsgi_app = socketio.WSGIApp(sio, app.wsgi_app)
 
 # pint.util.Quantity
 
-<<<<<<< Updated upstream
-def collbac(data):
-    print(data)
-    # print(type(str(data.value)))
-    # sio.emit("update_data", {"value": str(data.value)})
-    sio.emit("update_data", data)
-
-startAllWhach(collbac)
-
-@sio.event
-def connect(sid, environ, auth):
-    print('connect')
-
-
-
-@app.route("/a")
-def hello_world():
-    return "hellow world"
-=======
 ObdServisesUtils.startAllWhach(lambda nameSpace,data: sio.emit("update_data",data=data, namespace=nameSpace))
 
 @app.route("/get-all-errors")
@@ -38,4 +19,3 @@ def getAllErrors():
 @app.route("/reset-errors", methods=["POST"])
 def resetErrors():
     ObdServisesUtils.resetErrors()
->>>>>>> Stashed changes
--- SERVER/servises/ObdServisesUtils.py
+++ SERVER/servises/ObdServisesUtils.py
@@ -20,25 +20,9 @@ def startAllWhach(emitData):
     
     ObdConnection().connection.start()
 
-<<<<<<< Updated upstream
 
-
-# respons = None
-# for x in range(0x5f):
-#     respons[x] =  ObdConnection.Instanc().query(obd.commands[1][x], True).value
-
-# _connection = obd.OBD("COM7")
-# respons = dict( [(str(x),str(_connection.query(obd.commands[9][x], True).value)) for x in range(0x0b) ])
-# print(_connection.query(obd.commands.ECU_NAME, True).value)
-# file = open('./data/MODE1.json')
-# json.dump(file, respons)
-
-# with open("./data/MODE9.json", "w") as fid:
-#     json.dump(respons, fid, indent=4, sort_keys=True)
-=======
 def getAllErrors():
     return ObdConnection().connection.query(obd.commands.GET_DTC)
 
 def resetErrors():
     ObdConnection().connection.query(obd.commands.CLEAR_DTC)
->>>>>>> Stashed changes
